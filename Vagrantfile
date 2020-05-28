280effc7e342ea7d0e8436c6c0bbef2549ede216
dev-chulbuji <dev.chulbuji@gmail.com>
2020-05-28 23:43:43 +0900

    change bootstrap script

--- Vagrantfile
+++ Vagrantfile
@@ -1,12 +1,30 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
-#
+
+class String
+  def green
+    "\e[32m#{self}\e[0m"
+  end
+
+  def brown
+    "\e[33m#{self}\e[0m"
+  end
+end
+
+cmd = ARGV[0]
+
 VAGRANTFILE_API_VERSION = "2"
 VAGRANT_BOX_CENTOS_DOCKER = "centos/7"
 
 N1_HOST_NAME = "n1"
 
 N1_IP = "172.20.20.10"
+
+unless Dir.exist?("./vendor") and File.exist?("./vendor/done")
+  puts "You need to execute ./download_packages.sh before vagrant up. Execute the following line.".brown
+  puts "./download_packages.sh".brown
+  exit 1
+end
 
 Vagrant.configure(VAGRANTFILE_API_VERSION)do |config|
   config.vm.box = VAGRANT_BOX_CENTOS_DOCKER
@@ -23,8 +41,8 @@ Vagrant.configure(VAGRANTFILE_API_VERSION)do |config|
       n1.vm.hostname = N1_HOST_NAME
       n1.vm.network "private_network", ip: N1_IP
 
-      n1.vm.provision "file", source: "./src/packages", destination: "./tmp/src/packages"
-#      n1.vm.provision "shell", "./src/bootstrap.sh"
+      n1.vm.provision "file", source: "./vendor", destination: "/tmp/vendor"
+      n1.vm.provision "shell", path: "./src/bootstrap.sh"
 
       n1.vm.network "forwarded_port", guest: 80, host:8080
   end
--- download_packages.sh
+++ download_packages.sh
@@ -5,7 +5,7 @@ set -e
 VENDOR_DIR="./vendor"
 
 DOCKER_COMPOSE_VERSION="1.25.5"
-NODE_EXPORTER_VERSION="0.18.1"
+NODE_EXPORTER_VERSION="1.0.0"
 CADVISOR_VERSION="0.35.0"
 
 if [ -d $VENDOR_DIR ] 
@@ -16,13 +16,13 @@ fi
 mkdir -p $VENDOR_DIR
 
 curl -kL https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-Linux-x86_64 -o $VENDOR_DIR/docker-compose-$DOCKER_COMPOSE_VERSION
-curl -kL https://github.com/prometheus/node_exporter/releases/download/v$NODE_EXPORTER_VESION/node_exporter-$NODE_EXPORTER_VERSION.linux-amd64.tar.gz -o $VENDOR_DIR/node_exporter-$NODE_EXPORTER_VERSION.linux-amd64.tar.gz
-curl -kL https://github.com/google/cadvisor/releases/download/v$CADVISOR_VERSION.cadvisor -o $VENDOR_DIR/cadvisor-$CADVISOR_VERSION
+curl -kL https://github.com/google/cadvisor/releases/download/v$CADVISOR_VERSION/cadvisor -o $VENDOR_DIR/cadvisor-$CADVISOR_VERSION
+curl -kL https://github.com/prometheus/node_exporter/releases/download/v$NODE_EXPORTER_VERSION/node_exporter-$NODE_EXPORTER_VERSION.linux-amd64.tar.gz -o $VENDOR_DIR/node_exporter-$NODE_EXPORTER_VERSION.linux-amd64.tar.gz
 
 curl -kL https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.6-3.3.el7.x86_64.rpm -o $VENDOR_DIR/containerd.io-1.2.6-3.3.el7.x86_64.rpm
 curl -kL https://download.docker.com/linux/centos/7/x86_64/stable/Packages/docker-ce-19.03.9-3.el7.x86_64.rpm -o $VENDOR_DIR/docker-ce-19.03.9-3.el7.x86_64.rpm
 curl -kL https://download.docker.com/linux/centos/7/x86_64/stable/Packages/docker-ce-cli-19.03.9-3.el7.x86_64.rpm -o $VENDOR_DIR/docker-ce-cli-19.03.9-3.el7.x86_64.rpm
-curl -L https://download.docker.com/linux/centos/7/x86_64/stable/Packages/docker-ce-selinux-17.03.3.ce-1.el7.noarch.rpm -o $VENDOR_DIR/container-selinux-17.03.0.ce-1.el7.noarch.rpm
+curl -L http://mirror.centos.org/centos/7/extras/x86_64/Packages/container-selinux-2.119.1-1.c57a6f9.el7.noarch.rpm -o $VENDOR_DIR/container-selinux-2.119.1-1.c57a6f9.el7.noarch.rpm
 
 touch $VENDOR_DIR/done
 
